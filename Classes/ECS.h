5988cd80f27fe71e424b06aa269b83775c263dc3
karasato@square-enix.com <karasato@square-enix.com>
2018-05-18 19:30:31 +0900

    zz

--- Classes/ECS.h
+++ Classes/ECS.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include "ecs/ComponentRegistry.h"
-#include "component/ExplodeComponent.h"
+
 #include "component/HealthComponent.h"
 #include "component/LifetimeComponent.h"
 #include "component/RigidBodyComponent.h"
 #include "component/TransformComponent.h"
-#include "component/BallBehaviorComponent.h"
 #include "component/SpawnerComponent.h"
 
 #include "render_component/VisualComponent.h"
